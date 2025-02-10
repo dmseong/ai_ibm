2b68835492b894f3008f2ba770676de8ecbf04c1
Coda <onemore4111@gmail.com>
2025-02-10 20:06:40 +0900

    프론트 통신

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI, Request, BackgroundTasks
 # CORS 정책
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import os
+from fastapi import FastAPI, Form
+from dotenv import load_dotenv
+from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai.foundation_models.utils.enums import ModelTypes
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.foundation_models.utils.enums import DecodingMethods
+from model import Message, PromptMessage
 
 from dotenv import load_dotenv
 
@@ -45,6 +54,44 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-@app.get("/")
-def helloworld():
-    return {"Hello World"}
+
+def create_llm(api_key, api_url, project_id):
+   
+   parameters = { 
+        GenParams.DECODING_METHOD: DecodingMethods.GREEDY.value,
+        GenParams.MIN_NEW_TOKENS: 1,
+        GenParams.MAX_NEW_TOKENS: 500,
+        GenParams.STOP_SEQUENCES: ["<|endoftext|>"]
+    }
+   
+   credentials = Credentials(
+        url=api_url,
+        api_key=api_key
+    )
+
+   model_id =  'ibm/granite-3-8b-instruct' # 'mistralai/mistral-large' # 'meta-llama/llama-3-1-70b-instruct' # ModelTypes.LLAMA_2_70B_CHAT.value #
+   llm = ModelInference(
+        model_id=model_id,
+        params=parameters,
+        credentials=credentials,
+        project_id=project_id
+    ) 
+   
+   return llm
+
+api_key = os.environ["API_KEY"]
+api_url = os.environ["API_URL"]
+project_id = os.environ["PROJECT_ID"]
+
+
+model = create_llm(api_key, api_url, project_id)
+
+
+@app.post("/processing", description="prompt message",response_model = Message)
+def watsonx_ai_api(promptMessage: PromptMessage):
+    
+    response = model.generate(prompt=promptMessage.prompt)['results'][0]['generated_text'].strip()
+    print(response) 
+    msg = {"text": response}
+    
+    return msg
